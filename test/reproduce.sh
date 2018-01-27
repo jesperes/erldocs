50e4679b5cf5e4df578b4a1dff8b07d45e5b4a03
Pierre Fenoll <pierrefenoll@gmail.com>
2018-01-27 18:02:21 +0100

    testing: test/reproduce.sh

---
+++ test/reproduce.sh
@@ -0,0 +1,104 @@
+#!/bin/bash -eu
+
+[[ $# -ne 1 ]] && echo "Usage: $0  <kerl OTP release>" && exit 2
+rel=$1
+rel_path=test/rel
+ref_path=test/ref
+
+erldocs=_build/default/bin/erldocs
+[[ -x $erldocs ]] || exit 3
+
+kerl_path="$(which kerl)"
+kerl_bin=/usr/local/bin/kerl
+
+# cat <<EOF
+# #!/bin/bash -eux
+
+mkdir -p $rel_path $ref_path
+docker run --rm --interactive \
+       --volume "$PWD"/$erldocs:/app/erldocs:ro \
+       --volume $kerl_path:$kerl_bin:ro \
+       --volume "$PWD"/$rel_path:/rel:rw \
+       --volume "$PWD"/$ref_path:/ref:rw \
+       debian:8 \
+       /bin/bash -eux -s <<EOF
+#!/bin/bash
+
+# Setup
+apt-get update && apt-get upgrade -y
+apt-get install -y git curl build-essential libncurses-dev libssl-dev
+say() {
+    printf '\n\e[1;3m%s\e[0m\n' "\$*"
+}
+
+# Fetch reference build of docs
+odir=/ref/docs-$rel
+if [[ ! -f /ref/docs-$rel.tar.bz2 ]]; then
+    curl -#fSLo \$odir.tar.bz2 https://erldocs.com/archives/docs-$rel.tar.bz2
+    tar xaf \$odir.tar.bz2
+    ( cd \$odir
+      git init
+      git add -Af .
+      git commit -m ref
+    )
+fi
+
+
+# Fetch + build + install + build docs
+export KERL_BASE_DIR=/rel
+export KERL_BUILD_BACKEND=git
+if ! [[ -f \$KERL_BASE_DIR/otp_releases ]]; then
+    kerl update releases
+fi
+if ! grep -Fo '$rel' \$KERL_BASE_DIR/otp_releases >/dev/null; then
+    say Tag '$rel' is unknown to GitHub!
+    exit 1
+fi
+
+idir=\$KERL_BASE_DIR/builds/$rel/otp_src_$rel
+if [[ ! -d \$idir ]]; then
+    kerl build $rel $rel
+fi
+
+if [[ ! -f \$idir/lib/xmerl/doc/src/xmerl.xml ]]; then
+    ( cd \$idir
+      say Commencing build of $rel docs
+      if MAKEFLAGS=-j6 ./otp_build setup -a && MAKEFLAGS=-j6 make docs; then
+          say Built $rel docs
+      else
+          say Could not make $rel docs
+          exit 2
+      fi
+    )
+fi
+
+release_local_dir=\$KERL_BASE_DIR/$rel
+if [[ ! -d \$release_local_dir ]]; then
+    if ! kerl install $rel \$release_local_dir; then
+        say Could not install $rel in \$release_local_dir
+        exit 3
+    fi
+fi
+chown -R $(id -u):$(id -g) /rel
+say Using erl at "\$(which erl)"
+. \$release_local_dir/activate
+
+
+# Run erldocs
+for f in \$odir/* \$odir/.*; do
+    if [[ "\$f" = .git ]] || [[ "\$f" = . ]] || [[ "\$f" = .. ]]; then
+        continue
+    fi
+    rm -rf "\$f"
+done
+say Running erldocs...
+/app/erldocs -o \$odir \$idir/lib/* \$idir/erts* | tee _$rel
+chown -R $(id -u):$(id -g) /ref
+
+say Comparing against reference
+( cd \$odir
+  git status --porcelain
+  [[ '0' = "\$(git status --porcelain | wc -l)" ]]
+)
+
+EOF
